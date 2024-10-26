9bb37e88020046e23e03fef84ce6fabddff564e3
manoharn725 <manoharnmanu725@gmail.com>
2024-10-27 00:52:30 +0530

    paths got changed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productsReducer from "../slices/productsSlice";
-import cartItemsReducer from "../slices/cartItemsSlice";
-import wishListReducer from "../slices/wishListSlice";
+import productsReducer from "./slices/productsSlice";
+import cartItemsReducer from "./slices/cartItemsSlice";
+import wishListReducer from "./slices/wishListSlice";
 
 export const store = configureStore({
   reducer: {
