3f8a73aaeb1b32fc6bca7eb933a4b2d15da2f34c
manoharn725 <manoharnmanu725@gmail.com>
2024-10-27 00:50:09 +0530

    wishlist and cartItems slice

---
+++ src/redux/slices/cartItemsSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const cartItemsSlice = createSlice({
+  name: "cartItems",
+  initialState:[],
+  reducers: {
+    addCartItem: (state, action) => {
+      state.push(action.payload)
+    },
+    removeCartItem: (state, action) => {
+      return state.filter((item) => item.id !== action.payload);
+    },
+    increaseCartItemQuantity: (state, action) => {
+      console.log(action);
+      return state;
+    },
+    decreaseCartItemQuantity: (state, action) => {
+      console.log(action);
+      return state;
+    },
+  },
+});
+console.log(cartItemsSlice)
+export const { addCartItem, removeCartItem, increaseCartItemQuantity, decreaseCartItemQuantity } = cartItemsSlice.actions;
+
+export default cartItemsSlice.reducer;
